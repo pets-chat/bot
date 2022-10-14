880950d58bcb150f5ea76cbfcb339c76ae57de9d
megubyte <939805+megubyte@users.noreply.github.com>
2022-10-14 22:19:19 +0100

    allow capitals

--- petsbot/extensions/pronouns.py
+++ petsbot/extensions/pronouns.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext
 from telegram.error import BadRequest
 
 async def handle_pronouns_command(update: Update, context: CallbackContext):
-    if not re.match(r"^[a-z]+(\/[a-z]+){1,2}$", context.args[0]) or len(context.args[0]) > 16:
+    if not re.match(r"^[a-zA-Z]+(\/[a-zA-Z]+){1,2}$", context.args[0]) or len(context.args[0]) > 16:
         await update.message.reply_text("Must be under 16 characters, and have slashes in the middle (up to two).")
         return
 
